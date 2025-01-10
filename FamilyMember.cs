e2b0e22c8c1d0ce4d836eec1bf68d35b1a568a25
YuriyGlukhov <rols5@rambler.ru>
2025-01-10 18:24:31 +0300

    Add files via upload

---
+++ FamilyMember.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace FamilyMembers
+{
+    public enum Gender { male, female }
+    public class FamilyMember
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+
+        public FamilyMember Father { get; set; }
+        public FamilyMember Mother { get; set; }
+
+        public FamilyMember BrotherOrSister { get; set; } //добавление братьев и сестер
+
+
+        public FamilyMember Spouse { get; set; } // добавление супругов
+        public FamilyMember[] Children { get; set; } // добавление детей
+
+
+
+        public FamilyMember[] GetGranMotherName()
+        {
+            return new FamilyMember[] { Mother.Mother, Father.Mother };
+        }
+        public FamilyMember[] GetGranFatherName()
+        {
+            return new FamilyMember[] { Mother.Father, Father.Father };
+        }
+
+        public void MyDisplayFam()
+        {
+            DisplayGrandParents();
+            DisplayParents();
+            DisplayClidrenParrents();
+
+            DisplaySpouse();
+            DisplayChildren();
+
+
+            
+        }
+
+        public void DisplayGrandParents()
+        {
+
+            string paternalGrandFather = Father?.Father != null ? $"{Father.Father.Name}, {Father.Father.Age}, {Father.Father.Gender}" : "Дедушка по отцу: Неизвестен";
+            string paternalGrandMother = Father?.Mother != null ? $"{Father.Mother.Name}, {Father.Mother.Age}, {Father.Mother.Gender}" : "Бабушка по отцу: Неизвестна";
+            string maternalGrandFather = Mother?.Father != null ? $"{Mother.Father.Name}, {Mother.Father.Age}, {Mother.Father.Gender}" : "Дедушка по матери: Неизвестен";
+            string maternalGrandMother = Mother?.Mother != null ? $"{Mother.Mother.Name}, {Mother.Mother.Age}, {Mother.Mother.Gender}" : "Бабушка по матери: Неизвестна";
+
+            Console.WriteLine($"{new string(' ', 10)}Дедушка по отцу: {new string(' ', 18)}Бабушка по отцу:{new string(' ', 35)}Дедушка по матери{new string(' ', 18)}Бабушка по матери");
+            Console.WriteLine($"|{paternalGrandFather}|----|{paternalGrandMother}|{new string(' ', 9)}|{maternalGrandFather}|----|{maternalGrandMother}|");
+
+        }
+
+        public void DisplayParents()
+        {
+
+            string fatherInfo = Father != null ? $"{Father.Name}, {Father.Age}, {Father.Gender}" : "Отец: Неизвестен";
+            string motherInfo = Mother != null ? $"{Mother.Name}, {Mother.Age}, {Mother.Gender}" : "Мать: Неизвестна";
+
+            Console.WriteLine($"{new string(' ', 34)}|{new string(' ', 85)}|");
+            Console.WriteLine($"{new string(' ', 32)}Отец: {new string(' ', 81)}Мать:");
+            Console.WriteLine($"{new string(' ', 18)}|{fatherInfo}|{new string('-', 51)}|{motherInfo}|");
+        }
+
+        public void DisplayClidrenParrents()
+        {
+            if (BrotherOrSister != null)
+            {
+
+               
+                Console.WriteLine($"{new string(' ', 54)}|{new string(' ', 42)}|");
+                Console.WriteLine($"{new string(' ', 50)}Наш member{new string(' ', 32)}Брат/Сестра");
+                Console.WriteLine($"{new string(' ', 37)}**{Name},{Age},{Gender}**{new string(' ', 7)}|{BrotherOrSister.Name},{BrotherOrSister.Age},{BrotherOrSister.Gender}|");
+            }
+            else
+            {
+                Console.WriteLine($"{new string(' ', 54)}|{new string(' ', 42)}|");
+                Console.WriteLine($"{new string(' ', 50)}Наш member{new string(' ', 32)}Брат/Сестра");
+                Console.WriteLine($"{new string(' ', 37)}**{Name},{Age},{Gender}**{new string(' ', 17)}|Неизвестно|");
+            }
+           
+        }
+
+        public void DisplaySpouse()
+        {
+
+            if (Spouse != null)
+            {
+                Console.WriteLine($"{new string(' ', 35)}|{new string(' ', 18)}|");
+                Console.WriteLine($"{new string(' ', 30)}Супруг(а){ new string(' ', 15)}|");
+                Console.WriteLine($"{new string(' ', 15)}|{Spouse.Name}, {Spouse.Age}, {Spouse.Gender}|{new string(' ', 1)}|");
+            }
+            else
+            {
+                Console.WriteLine($"{new string(' ', 35)}|{new string(' ', 18)}|");
+                Console.WriteLine($"{new string(' ', 30)}Супруг(а){new string(' ', 15)}|");
+                Console.WriteLine($"{new string(' ', 30)}Неизвестно{new string(' ', 14)}|");
+            }
+        }
+        public void DisplayChildren()
+        {
+           
+            if (Children != null && Children.Length > 0)
+            {
+                Console.WriteLine($"{new string(' ', 54)}|");
+                Console.WriteLine($"{new string(' ', 53)}Дети:");
+                
+                foreach (var child in Children)
+                {
+                    Console.WriteLine($"{new string(' ', 40)}{child.Name}, {child.Age}, {child.Gender}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{new string(' ', 54)}|");
+                Console.WriteLine($"{new string(' ', 53)}Дети:");
+                Console.WriteLine($"{new string(' ', 50)}|Неизвестно|");
+
+
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq.Expressions;
+using FamilyMembers;
+
+namespace C_Immersive_Seminar1_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FamilyMember grandFatrher_1 = new FamilyMember()
+            {
+                Name = "Мухов Олег Витальевич",
+                Age = 61,
+                Gender = Gender.male,
+              
+            };
+
+            FamilyMember grandFatrher_2 = new FamilyMember()
+            {
+                Name = "Шушукин Борис Юрьевич",
+                Age = 63,
+                Gender = Gender.male
+            };
+
+            FamilyMember grandMother_1 = new FamilyMember()
+            {
+                Name = "Мухова Валентина Евгеневна",
+                Age = 59,
+                Gender = Gender.female
+            };
+
+            FamilyMember grandMother_2 = new FamilyMember()
+            {
+                Name = "Шушукина Роза Олеговна",
+                Age = 66,
+                Gender = Gender.female
+            };
+
+            FamilyMember father = new FamilyMember()
+            {
+                Name = "Мухов Кирилл Олегович",
+                Age = 45,
+                Gender = Gender.male,
+                Mother = grandMother_1,
+                Father = grandFatrher_1,
+              
+            };
+            FamilyMember mother = new FamilyMember()
+            {
+                Name = "Мухова Татьяна Борисовна",
+                Age = 43,
+                Gender = Gender.female,
+                Mother = grandMother_2,
+                Father = grandFatrher_2,
+                
+            };
+
+            FamilyMember brother = new FamilyMember()
+            {
+                Name = "Мухов Вдалислав Кириллович",
+                Age = 15,
+                Gender = Gender.male,
+                Mother = mother,
+                Father = father,
+            };
+            FamilyMember wife = new FamilyMember()
+            {
+                Name = "Тестовая Жена Тестововна",
+                Age = 20,
+                Gender = Gender.female,
+               
+            };
+
+            FamilyMember member = new FamilyMember()
+            {
+                Name = "Мухов Александр Кириллович",
+                Age = 21,
+                Gender = Gender.male,
+                Mother = mother,
+                Father = father,
+            };
+            FamilyMember Son = new FamilyMember()
+            {
+                Name = "Мухов Олег Александрович",
+                Age = 2,
+                Gender = Gender.male,
+                Mother = wife,
+                Father = member
+            };
+
+            member.BrotherOrSister = brother;
+            member.Children = new FamilyMember[] {Son };
+            member.Spouse = wife;
+
+
+            member.MyDisplayFam();
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
